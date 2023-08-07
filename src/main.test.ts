0f94d692be31539013f9815874df091ea1e6e6aa
messabih <msbih.khalil@gmail.com>
2023-08-07 23:11:29 +0100

    test(main.test.ts): add test case to check if the server greets with the correct message test(main.test.ts): update the endpoint in the test case for posting a new user to match the actual endpoint

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,12 +1,19 @@
 import { expect, it } from 'vitest';
 import axios from 'axios';
+
+it('should greeting', async () => {
+    const res = await axios.get('http://127.0.0.1:3000/');
+
+    expect(res.data).toEqual('Hello, TypeScript Express!');
+});
+
 it('should post new user', async () => {
     const data = {
         name: 'messabih',
         email: 'messabih@gmail.com',
     };
 
-    const res = await axios.post('http://127.0.0.1:3000', data);
+    const res = await axios.post('http://127.0.0.1:3000/users', data);
 
     expect(res.status).toEqual(201);
 });
